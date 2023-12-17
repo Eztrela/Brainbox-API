aaef2a127f43d6604c9c1c66a56a733a5be91512
Lucas Nobrega <lucaspdn04@gmail.com>
2023-12-17 14:38:47 -0300

    initial User and UserRepository

---
+++ src/main/java/api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package api.repository;
+
+import api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByUsername(String username);
+
+    public User findByEmail(String email);
+}
