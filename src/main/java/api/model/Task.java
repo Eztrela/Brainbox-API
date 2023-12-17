28a0dafa781878bfba0c18edb70fb184ef3ca266
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-12-17 11:31:29 -0300

    adicionando modelo e mapeamento de Task

--- src/main/java/api/model/Task.java
+++ src/main/java/api/model/Task.java
@@ -1,5 +1,106 @@
 package api.model;
 
-public class Task {
+import jakarta.persistence.*;
 
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private Date datetimeCreated;
+    private Date datetimeDue;
+    private Long priority;
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
+    private Tag tag;
+
+    public Task(String title, String description, String status, Date datetimeCreated, Date datetimeDue, Long priority, Tag tag) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.datetimeCreated = datetimeCreated;
+        this.datetimeDue = datetimeDue;
+        this.priority = priority;
+        this.tag = tag;
+    }
+
+    public Task() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDatetimeDue() {
+        return datetimeDue;
+    }
+
+    public void setDatetimeDue(Date datetimeDue) {
+        this.datetimeDue = datetimeDue;
+    }
+
+    public Long getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Long priority) {
+        this.priority = priority;
+    }
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Date getDatetimeCreated() {
+        return datetimeCreated;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                ", datetimeCreated=" + datetimeCreated +
+                ", datetimeDue=" + datetimeDue +
+                ", priority=" + priority +
+                ", tag=" + tag.getTitle() +
+                '}';
+    }
 }
