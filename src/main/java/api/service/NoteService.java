96dbb650b4db8bbd883d5da60a2a94a92ae738e0
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-12-18 22:55:23 -0300

    criando serviço de Note

---
+++ src/main/java/api/service/NoteService.java
@@ -0,0 +1,52 @@
+package api.service;
+
+import api.model.Note;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NoteService {
+
+    @Autowired
+    private NoteRepository noteRepository;
+
+    public List<Note> getAll(){
+        return noteRepository.findAll().stream().toList();
+    }
+
+    public Note getById(Long id){
+        return noteRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public Note insert(Note note){
+        Optional<Note> register = noteRepository.findByContent(note.getContent());
+        if (register.isPresent()) throw new RuntimeException("Note already exists");
+        return noteRepository.save(note);
+    }
+
+    @Transactional
+    public Note update(Note note){
+        return noteRepository..findById(note.getId()).map(
+                register -> {
+                    register.setContent(note.getContent());
+                    return noteRepository.save(register);
+                }
+        ).orElseThrow(() -> new RuntimeException("Note not found"));
+    }
+
+    public void delete(Long id){
+        Optional<Note> register = noteRepository.findById(id);
+        if (register.isEmpty()) throw new RuntimeException("Note " + id + " not found");
+        noteRepository.deleteById(id);
+    }
+
+    public Note findByContent(String title){
+        return noteRepository.findByContent(title).orelse(null);
+    }
+
+}
