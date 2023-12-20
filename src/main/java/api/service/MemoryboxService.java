b57074c8237ef707d67ce43b09f77e8bdd6af273
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-12-20 14:14:46 -0300

    TASK67 - Criar Serviço de Memorybox (#11)
    * criando service de memorybox
    
    * mudando metodos do serviço de memorybox insert e update para usar dto e id no update
    
    ---------
    
    Co-authored-by: Pablo Estrela <pablo.estrela@assert.ifpb.edu.br>

---
+++ src/main/java/api/service/MemoryboxService.java
@@ -0,0 +1,56 @@
+package api.service;
+
+import api.model.Memorybox;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemoryboxService {
+    @Autowired
+    private MemoryboxRepository memoryboxRepository;
+
+    public List<Memorybox> getAll(){
+        return memoryboxRepository.findAll().stream().toList();
+    }
+
+    public Memorybox getById(Long id){
+        return memoryboxRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public Memorybox insert(MemoryboxInsertDTO memorybox){
+        Optional<Memorybox> register = memoryboxRepository.findByTitle(memorybox.getTitle());
+        if(register.isPresent()) throw new RuntimeException("Memorybox already exists");
+        return memoryboxRepository.save(memorybox);
+    }
+
+    @Transactional
+    public Memorybox update(MemoryboxInsertDTO memorybox, Long id){
+        return memoryboxRepository.findById(id).map(
+                register -> {
+                    register.setTitle(memorybox.getTitle());
+                    register.setBanner(memorybox.getBanner());
+                    register.setTasks(memorybox.getTasks());
+                    register.setNotes(memorybox.getNotes());
+                    register.setTags(memorybox.getTags());
+                    return memoryboxRepository.save(register);
+                }
+        ).orElseThrow(() -> new RuntimeException("Memorybox not found"));
+    }
+
+    public void delete(Long id){
+        Optional<Memorybox> register = memoryboxRepository.findById(id);
+        if(register.isEmpty()) throw new RuntimeException("Memorybox " + id + " not found");
+        memoryboxRepository.deleteById(id);
+    }
+
+    public Memorybox findByTitle(String title){
+        return memoryboxRepository.findByTitle(title).orElse(null);
+    }
+
+
+}
