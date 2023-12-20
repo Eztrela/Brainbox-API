e5068c64b189589a164c9b6380c27c04259a3870
Pablo Estrela <pablo.estrela@assert.ifpb.edu.br>
2023-12-20 13:41:30 -0300

    alterando metodos do repositorio para retornar um Optional

--- src/main/java/api/repository/TagRepository.java
+++ src/main/java/api/repository/TagRepository.java
@@ -4,9 +4,10 @@ import api.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
     public Tag findByTitle(String title);
 
-    public List<Tag> findByColor(String color);
+    public Optional<Tag> findByColor(String color);
 }
